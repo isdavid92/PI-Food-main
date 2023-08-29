94f309885c1bb165d3b77a4c7efc2563e33456ad
isdavid92 <isdavid92@hotmail.com>
2023-08-29 13:43:00 -0500

    listo el backend

--- api/src/controllers/postRecipe.js
+++ api/src/controllers/postRecipe.js
@@ -1,61 +1,59 @@
+const cheerio = require('cheerio');
 const errorHandler = require('../utils/errors');
 const { Recipe, Diet } = require('../db');
+const getDiets = require('./getDiets');
 
 const postRecipe = async (req, res) => {
+
     const idDB = async () => {
-        const idSerch = async () => {
-            const recipesDB = await Recipe.findAll();
-            let idM = 1165540;
-            if (recipesDB.length) {
-                for (let i = 1; i < recipesDB.length+1; i++) {
-                    let pos = i*2;
-                    idM += pos
-                };
-            };
-            return idM
-        };
-        const idDB = await idSerch();
-        return idDB
+        const recipesDB = await Recipe.findAll();
+        let idM = 1165540;
+        if (!recipesDB.length) return idM;
+            
+        return idM += recipesDB.length * 2
     };
 
-    // const loadDiets = async () => {
-    //     const dietsDB = await Diet.findAll();
-    //     if(dietsDB.length) return 
-    //      fetch('https://spoonacular.com/food-api/docs/diet-definitions')
-    //     .then(response => response.text())
-    //     .then(text => {
-    //             const $ = cheerio.load(text);
-    //             const response = $('ul').map((i, elem) => {
-    //                 return {
-    //                     items: $(elem).find('li').map((j, item) => $(item).text()).get()
-    //                 };
-    //             }).get();
-    //             const diets = [];
-    //             const dietsString = response[3].items;
-    //             dietsString.map((string) => {
-    //                 const splitString = string.split(':');
-    //                 diets.push({
-    //                     name: splitString[0],
-    //                     info: splitString[1].trim()
-    //                 });
-    //             });
-    //             diets.map( async ({ name, info }) => {
-    //                 await Diet.create({ name, info })
-    //             });
-    //             return diets
-    //         });
-    // };
-
     try {
-        // loadDiets()
+        fetch('https://spoonacular.com/food-api/docs/diet-definitions')
+        .then(response => response.text())
+        .then( async (text) => {
 
-        const { title, image, summary, health_score, steps, diets } = req.body;
-        const id = await idDB();
-        const newRecipe = await Recipe.create({ id, title, image, summary, health_score, steps });
+            const dietsDB = await Diet.findAll();
 
-        newRecipe.addDiet(diets);
-        
-        res.status(201).json(newRecipe)
+            if (!dietsDB.length) {
+
+                const $ = cheerio.load(text);
+                const dietsDB = [];
+                const response = $('ul').map((i, elem) => {
+                    return {
+                        items: $(elem).find('li').map((j, item) => $(item).text()).get()
+                    };
+                }).get();
+                const dietsString = response[3].items;
+                dietsString.map((string) => {
+                    const splitString = string.split(':');
+                    dietsDB.push({
+                        name: splitString[0],
+                        info: splitString[1].trim()
+                    });
+                });
+                dietsDB.map(async ({ name, info }) => {
+                    await Diet.create({ name, info })
+                });
+                console.log('BASE DE DATOS PRECARGADA CON LAS DIETAS')
+
+            };
+            
+            console.log('DIETAS EN BASE DE DATOS')
+            const { title, image, summary, health_score, steps, diets } = req.body;
+            const id = await idDB();
+            const newRecipe = await Recipe.create({ id, title, image, summary, health_score, steps });
+    
+            newRecipe.addDiet(diets);
+            
+            res.status(201).json(newRecipe)
+            
+        });
     } catch (error) {
         errorHandler(res, error)
     }
